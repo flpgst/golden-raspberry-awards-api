68844c45e8185659bc230a43a99d5e6e4f3c04dc
Filipe Augusto Gonçalves <flpgst@gmail.com>
2021-06-20 02:02:56 -0300

    MovieList creation moved to MovieModule

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,18 +1,4 @@
-import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
-import { createReadStream } from 'fs';
-import * as csv from 'csv-parser';
-// const movielist = require('./movielist.csv');
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService implements OnApplicationBootstrap {
-  onApplicationBootstrap(): void {
-    console.log(__dirname, 'movielist.csv');
-    createReadStream(`${__dirname}/movielist.csv`)
-      .pipe(
-        csv({
-          separator: ';',
-        }),
-      )
-      .on('data', (movie) => console.log(movie));
-  }
-}
+export class AppService {}
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,26 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateMovieDto } from './dto/create-movie.dto';
+import { MovieEntity } from './entities/movie.entity';
+import { createReadStream } from 'fs';
+import * as csv from 'csv-parser';
 // import { UpdateMovieDto } from './dto/update-movie.dto';
 
 @Injectable()
-export class MoviesService {
-  create(createMovieDto: CreateMovieDto) {
-    return 'This action adds a new movie';
+export class MoviesService implements OnApplicationBootstrap {
+  constructor(
+    @InjectRepository(MovieEntity)
+    private movieRepository: Repository<MovieEntity>,
+  ) {}
+
+  async onApplicationBootstrap() {
+    const movieList = await this.findAll();
+    if (!movieList.length)
+      createReadStream(`${__dirname}/../config/movielist.csv`)
+        .pipe(
+          csv({
+            separator: ';',
+          }),
+        )
+        .on('data', (movie) => {
+          this.create(movie);
+        })
+        .on('end', () => console.log('Movie list updated from CSV File'));
   }
 
-  findAll() {
-    return `This action returns all movies`;
+  async create(createMovieDto: CreateMovieDto): Promise<MovieEntity> {
+    return await this.movieRepository.save(createMovieDto);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} movie`;
+  async findAll(): Promise<MovieEntity[]> {
+    return await this.movieRepository.find();
+  }
+
+  async findOne(id: number): Promise<MovieEntity> {
+    return this.movieRepository.findOne(id);
   }
 
   // update(id: number, updateMovieDto: UpdateMovieDto) {
   //   return `This action updates a #${id} movie`;
   // }
 
-  remove(id: number) {
-    return `This action removes a #${id} movie`;
+  async remove(id: number) {
+    // const movie = this.findOne(id);
+    // if (!movie) return;
+    return await this.movieRepository.delete(id);
   }
 }
